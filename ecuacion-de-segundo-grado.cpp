fc9635cfde6012d986b1f08176f742380d60f648
Alejandro <alex.mac.fon@gmail.com>
2023-03-14 23:58:32 -0600

    Cambio en el nombre de la variable, x_n = Solucion_n

--- ecuacion-de-segundo-grado.cpp
+++ ecuacion-de-segundo-grado.cpp
@@ -5,23 +5,23 @@
 
 using namespace std;
 
-float a, b, c, Radicando, x_1, x_2;
+float a, b, c, Radicando, Solucion1, Solucion2;
 
 void ValorImaginario() {
 
-    x_1 = (-b)/(2*a);
-    x_2 = (sqrt(-Radicando))/(2*a);
+    Solucion1 = (-b)/(2*a);
+    Solucion2 = (sqrt(-Radicando))/(2*a);
 
-    if (x_2 > 0) {
+    if (Solucion2 > 0) {
 
-        cout << "x_1 = " << x_1 << " +" << x_2 << "i" << endl;
-        cout << "x_2 = " << x_1 << " " << -x_2 << "i" << endl;
+        cout << "x_1 = " << Solucion1 << " +" << Solucion2 << "i" << endl;
+        cout << "x_2 = " << Solucion1 << " " << -Solucion2 << "i" << endl;
     }
 
     else {
 
-        cout << "x_1 = " << x_1 << " " << x_2 << endl;
-        cout << "x_2 = " << x_2 << " +" << -x_2 << endl;
+        cout << "x_1 = " << Solucion1 << " " << Solucion2 << endl;
+        cout << "x_2 = " << Solucion2 << " +" << -Solucion2 << endl;
     }
 }
 
@@ -35,8 +35,8 @@ int main (int argc, char *argv[]) {
     Radicando = (pow(b,2))-(4*a*c);
 
     if (a == 0) {
-        x_1 = -c/b;
-        cout << "x = " << x_1;
+        Solucion1 = -c/b;
+        cout << "x = " << Solucion1;
     }
 
     else if (Radicando < 0) {
@@ -44,16 +44,16 @@ int main (int argc, char *argv[]) {
     }
 
     else if (Radicando == 0) {
-        x_1 = (-b)/(2*a);
-        cout << "x = " << x_1;
+        Solucion1 = (-b)/(2*a);
+        cout << "x = " << Solucion1;
     }
 
     else {
-        x_1 = (-b + sqrt(Radicando))/(2*a);
-        x_2 = (-b - sqrt(Radicando))/(2*a);
+        Solucion1 = (-b + sqrt(Radicando))/(2*a);
+        Solucion2 = (-b - sqrt(Radicando))/(2*a);
 
-        cout << "x_1 = " << x_1 << endl;
-        cout << "x_2 = " << x_2 << endl;
+        cout << "x_1 = " << Solucion1 << endl;
+        cout << "x_2 = " << Solucion2 << endl;
     }
 
     return 0;
