aac9c3d17786b09a8f266e3e7524b78e6ddcdf23
Alejandro <alex.mac.fon@gmail.com>
2023-03-18 12:48:26 -0600

    Adicion del archivo para ordenar numeros

---
+++ sorting-numbers.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <math.h>
+#include <cmath>
+
+using namespace std;
+
+int Contador, Num1, Num2, NumerosEvaluados, ValorMinimo;
+
+int main (int argc, char *argv[]) {
+
+    for (Contador = 1; Contador <= NumerosEvaluados; Contador++) {
+
+        cout << "Numero " << Contador << " = ";
+        cin >> Num1;
+    }
+    return 0;
+}
