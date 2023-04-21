f14fb29b686bf81dfa54e1c0c8a0f261a6e2f037
Alejandro <alex.mac.fon@gmail.com>
2023-04-20 18:41:43 -0600

    indentadq

--- suma-vectores.cpp
+++ suma-vectores.cpp
@@ -6,54 +6,54 @@ using namespace std;
 
 int main () {
 
-double const PI = 3.141592;
-int NumeroDatos, ConstanteAngulo, MedidaGrados;
+   double const PI = 3.141592;
+   int NumeroDatos, ConstanteAngulo, MedidaGrados;
 
-cout << "Numero de datos: "; cin >> NumeroDatos; cout << endl << endl;
+   cout << "Numero de datos: "; cin >> NumeroDatos; cout << endl << endl;
 
-float DireccionVector[NumeroDatos], MagnitudVector[NumeroDatos], ComponenteX[NumeroDatos], ComponenteY[NumeroDatos], SumaComponentesX, SumaComponentesY, Resultante, Angulo;
+   float DireccionVector[NumeroDatos], MagnitudVector[NumeroDatos], ComponenteX[NumeroDatos], ComponenteY[NumeroDatos], SumaComponentesX, SumaComponentesY, Resultante, Angulo;
 
-for (int i = 0; i < NumeroDatos; i++) {
+   for (int i = 0; i < NumeroDatos; i++) {
 
-   cout << "Unidad de grados:" << endl << "1: grados" << endl << "2: radianes" << endl; cin >> MedidaGrados; cout << endl;
-   cout << "Direccion Vector " << i + 1 << ": "; cin >> DireccionVector[i]; cout << endl;
-   cout << "Magnitud Vector " << i + 1 << ": "; cin >> MagnitudVector[i]; cout << endl;
+      cout << "Unidad de grados:" << endl << "1: grados" << endl << "2: radianes" << endl; cin >> MedidaGrados; cout << endl;
+      cout << "Direccion Vector " << i + 1 << ": "; cin >> DireccionVector[i]; cout << endl;
+      cout << "Magnitud Vector " << i + 1 << ": "; cin >> MagnitudVector[i]; cout << endl;
 
-   if (MedidaGrados == 1) {
-      DireccionVector[i] = DireccionVector[i] * (PI / 180);
+      if (MedidaGrados == 1) {
+         DireccionVector[i] = DireccionVector[i] * (PI / 180);
+      }
+
+      ComponenteX[i] = MagnitudVector[i] * cos(DireccionVector[i]);
+      ComponenteY[i] = MagnitudVector[i] * sin(DireccionVector[i]);
+
+      SumaComponentesX += ComponenteX[i];
+      SumaComponentesY += ComponenteY[i];
+
    }
 
-   ComponenteX[i] = MagnitudVector[i] * cos(DireccionVector[i]);
-   ComponenteY[i] = MagnitudVector[i] * sin(DireccionVector[i]);
+   Resultante = sqrt(pow(SumaComponentesX,2) + pow(SumaComponentesY,2));
 
-   SumaComponentesX += ComponenteX[i];
-   SumaComponentesY += ComponenteY[i];
+   if (SumaComponentesX == 0) {
+      Angulo = (PI / 2);
+   }
+   else {
+      Angulo = atan(SumaComponentesY/SumaComponentesX);
+   }
 
-}
+   if (SumaComponentesX < 0) {
 
-Resultante = sqrt(pow(SumaComponentesX,2) + pow(SumaComponentesY,2));
+      Angulo += PI;
+   }
+   else {
 
-if (SumaComponentesX == 0) {
-   Angulo = (PI / 2);
-}
-else {
-   Angulo = atan(SumaComponentesY/SumaComponentesX);
-}
+      Angulo += 2*PI;
+   }
+   if (Angulo > 2*PI) {
 
-if (SumaComponentesX < 0) {
+      Angulo -= 2*PI;
+   }
 
-   Angulo += PI;
-}
-else {
-
-   Angulo += 2*PI;
-}
-if (Angulo > 2*PI) {
-
-   Angulo -= 2*PI;
-}
-
-cout << "Resultante: " << Resultante << endl << "Direccion: " << endl << Angulo << " radianes" << endl << Angulo * (180 / PI) << "grados";
-cout << endl;
-return 0;
+   cout << "Resultante: " << Resultante << endl << "Direccion: " << endl << Angulo << " radianes" << endl << Angulo * (180 / PI) << "grados";
+   cout << endl;
+   return 0;
 }
