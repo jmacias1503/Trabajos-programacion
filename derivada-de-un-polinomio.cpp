715e3a4a3ae899dbd5d108e35bfdf336c6f2fc1c
Alejandro <alex.mac.fon@gmail.com>
2023-04-27 19:03:16 -0600

    ahi va quedando la chingadera

---
+++ derivada-de-un-polinomio.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <cmath>
+#include <string>
+using namespace std;
+
+int main () {
+   int Terminos, Grado[Terminos];
+
+   cout << "Terminos: "; cin >> Terminos;
+   float Coeficiente[Terminos];
+   string valor[Terminos];
+
+
+   for (int i = 0; i < Terminos; i++) {
+      valor[i] = "+";
+      cin >> Coeficiente[i]; 
+      cout <<"\e[A" << Coeficiente[i] << "x^";
+      cin >> Grado[i];
+      if (Coeficiente[i] < 0) {
+         valor[i] = "-";
+      }
+   }
+   cout << "f(x) = " << Coeficiente[0] << "x^" << Grado[0] << " ";
+   for (int i =1; i < Terminos; i++) {
+      cout << valor[i] << abs(Coeficiente[i]) <<"x^" << Grado[i] << " ";
+   }
+   cout << "\nf'(x) = " << Coeficiente[0] * Grado[0] << "x^" << Grado[0] - 1 << " ";
+   for (int i = 1; i < Terminos; i++) {
+      cout << valor[i] << abs(Coeficiente[i]) * Grado[i] << "x^" << Grado[i] -1 << " ";
+   }
+}
