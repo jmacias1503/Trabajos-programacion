8799612e21bd67cf2f5c2e601abb37192007402f
Alejandro <alex.mac.fon@gmail.com>
2023-04-27 21:30:20 -0600

    actualizacion a nuevo codigo

--- derivada-de-un-polinomio-simple.cpp
+++ derivada-de-un-polinomio-simple.cpp
@@ -3,6 +3,13 @@
 #include <string>
 using namespace std;
 
+string Signo(float c[], int i) {
+   string v = "+";
+   if (c[i] < 0) {
+      v = "-";
+   }
+   return v;
+}
 int main () {
    int n, e[n];
 
@@ -10,15 +17,11 @@ int main () {
    float c[n];
    string v[n];
 
-
    for (int i = 0; i < n; i++) {
-      v[i] = "+";
       cin >> c[i]; 
       cout <<"\e[A" << c[i] << "x^";
       cin >> e[i];
-      if (c[i] < 0) {
-         v[i] = "-";
-      }
+      v[i] = Signo( c, i);
    }
    cout << "f(x) = " << c[0] << "x^" << e[0] << " ";
    for (int i =1; i < n; i++) {
@@ -26,14 +29,10 @@ int main () {
    }
    cout << "\nf'(x) = " << c[0] * e[0] << "x^" << e[0] - 1 << " ";
    for (int i = 1; i < n; i++) {
-      v[i] = "+";
       c[i] *= e[i];
       e[i]--;
-      if (c[i] < 0) {
-         v[i] = "-";
-      }
+      v[i] = Signo(c, i);
       cout << v[i] << abs(c[i]) << "x^" << e[i] << " ";
    }
    cout << endl;
 }
-
