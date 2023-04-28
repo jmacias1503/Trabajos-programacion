952f4a6ffafbbce30578bdf3cc2177beacfa31be
Alejandro <alex.mac.fon@gmail.com>
2023-04-28 09:24:17 -0600

    adicion de funcion para imprimir

--- derivada-de-un-polinomio.cpp
+++ derivada-de-un-polinomio.cpp
@@ -3,6 +3,22 @@
 #include <string>
 using namespace std;
 
+void Literales(float Coeficiente[], int i, float Grado[], string valor){
+   if (Coeficiente[i] == 0) {
+   }
+   else {
+   cout << valor << abs(Coeficiente[i]);
+   }
+   if (Grado[i] == 0) {
+   }
+   else if (Grado[i]==1) {
+      cout << "x";
+   }
+   else {
+      cout << "x^" << Grado[i];
+   }
+}
+
 string Signo(float Coeficiente[], int i) {
    string valor = "+";
    if (Coeficiente[i] < 0) {
@@ -12,10 +28,10 @@ string Signo(float Coeficiente[], int i) {
 }
 
 int main () {
-   int Terminos, Grado[Terminos];
+   int Terminos;
 
    cout << "Terminos: "; cin >> Terminos;
-   float Coeficiente[Terminos];
+   float Coeficiente[Terminos], Grado[Terminos];
    string valor[Terminos];
 
    for (int i = 0; i < Terminos; i++) {
@@ -26,14 +42,14 @@ int main () {
    }
    cout << "f(x) = " << Coeficiente[0] << "x^" << Grado[0] << " ";
    for (int i =1; i < Terminos; i++) {
-      cout << valor[i] << abs(Coeficiente[i]) <<"x^" << Grado[i] << " ";
+      //cout << valor[i] << abs(Coeficiente[i]) <<"x^" << Grado[i] << " ";
    }
    cout << "\nf'(x) = " << Coeficiente[0] * Grado[0] << "x^" << Grado[0] - 1 << " ";
    for (int i = 1; i < Terminos; i++) {
       Coeficiente[i] *= Grado[i];
       Grado[i]--;
       valor[i] = Signo(Coeficiente, i);
-      cout << valor[i] << abs(Coeficiente[i]) << "x^" << Grado[i] << " ";
+      //cout << valor[i] << abs(Coeficiente[i]) << "x^" << Grado[i] << " ";
    }
    cout << endl;
 }
