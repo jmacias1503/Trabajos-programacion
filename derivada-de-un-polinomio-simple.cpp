2d94a1d70e4d2083c8529ca9e846aa63e0188f86
Alejandro <alex.mac.fon@gmail.com>
2023-05-04 12:48:03 -0600

    actualizacion de codigo

--- derivada-de-un-polinomio-simple.cpp
+++ derivada-de-un-polinomio-simple.cpp
@@ -2,17 +2,16 @@
 #include <iostream>
 #include <string>
 using namespace std;
+
 int Literales(float c[], int i, float g[], string v[]){
+
    if (c[i] == 0) {
       return 0;
    }
    if (i!=0) {
       cout << v[i];
-      c[i] = abs(c[i]);
    }
-   if (c[i] != 1 && g[i] != 0) {
-      cout << c[i];
-   }
+      cout << abs(c[i]);
    if (g[i] != 0) {
       cout << "x";
    }
@@ -22,7 +21,9 @@ int Literales(float c[], int i, float g[], string v[]){
    cout << " ";
    return 0;
 }
+
 void SumaTerminos (float c[], float g[], int n, int i){
+
    for (i = 0; i < n ; i++) {
       for (int j = i + 1; j < n; j++) {
          if (g[i] == g [j]) {
@@ -32,6 +33,7 @@ void SumaTerminos (float c[], float g[], int n, int i){
       }
    }
 }
+
 string Signo(float c[], int i) {
    string v = "+";
    if (c[i] < 0) {
@@ -41,19 +43,20 @@ string Signo(float c[], int i) {
 }
 int main () {
    int n, i;
+
    cout << "Terminos: "; cin >> n;
    float c[n], g[n];
    string v[n];
+
    for (i = 0; i < n; i++) {
-      cout << "Termino " << i + 1 << ": " << endl;
+      cout << "Termino" << i + 1 << ": " << endl;
       cin >> c[i]; 
       cout <<"\e[A" << c[i] << "x^";
       cin >> g[i];
       v[i] = Signo( c, i);
       cout << endl;
    }
-   cout << "f(x) =";
-   SumaTerminos(c, g, n, i);
+   cout << "f(x) = ";
    for (int i =0; i < n; i++) {
    Literales(c, i, g, v);
    }
