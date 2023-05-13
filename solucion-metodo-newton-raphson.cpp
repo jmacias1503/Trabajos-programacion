cc1085e2664491e50a1389bf75a3575406fc8e57
Alejandro <alex.mac.fon@gmail.com>
2023-05-12 22:36:27 -0600

    problema de signos y exponente "solucionado"?

--- solucion-metodo-newton-raphson.cpp
+++ solucion-metodo-newton-raphson.cpp
@@ -21,10 +21,10 @@ int Literales(float Coeficiente[], int i, float Grado[], string valor[]){
    return 0;
 }
 
-void SumaTerminos (float Coeficiente[], float Grado[], int Terminos, int i){
+void SumaTerminos (float Coeficiente[], float Grado[], int terminos, int i){
 
-   for (i = 0; i < Terminos ; i++) {
-      for (int j = i + 1; j < Terminos; j++) {
+   for (i = 0; i < terminos ; i++) {
+      for (int j = i + 1; j < terminos; j++) {
          if (Grado[i] == Grado [j]) {
             Coeficiente[i] += Coeficiente[j];
             Coeficiente[j] = 0;
@@ -41,13 +41,13 @@ string Signo(float Coeficiente[], int i) {
    return valor;
 }
 int main () {
-   int Terminos, i;
+   int terminos, i;
 
-   cout << "Terminos: "; cin >> Terminos;
-   float Coeficiente[Terminos], Grado[Terminos], DerivadaCoeficiente[Terminos], DerivadaGrado[Terminos];
-   string valor[Terminos], DerivadaValor[Terminos];
+   cout << "terminos: "; cin >> terminos;
+   float Coeficiente[terminos], Grado[terminos], derivadaCoeficiente[terminos], derivadaGrado[terminos];
+   string valor[terminos], derivadaValor[terminos];
 
-   for (i = 0; i < Terminos; i++) {
+   for (i = 0; i < terminos; i++) {
       cout << "Termino" << i + 1 << ": " << endl;
       cin >> Coeficiente[i]; 
       cout <<"\e[A" << Coeficiente[i] << "x^";
@@ -56,34 +56,34 @@ int main () {
       cout << endl;
    }
    cout << "f(x) = ";
-   for (int i =0; i < Terminos; i++) {
+   for (int i =0; i < terminos; i++) {
    Literales(Coeficiente, i, Grado, valor);
    }
    cout << "\nf'(x) = " ;
-   for (i = 0; i < Terminos; i++) {
-      DerivadaCoeficiente[i] = Coeficiente[i] * Grado[i];
-      DerivadaGrado[i] = Grado[i]--;
-      valor[i] = Signo(Coeficiente, i);
-      Literales(DerivadaCoeficiente, i, DerivadaGrado, DerivadaValor);
+   for (i = 0; i < terminos; i++) {
+      derivadaCoeficiente[i] = Coeficiente[i] * Grado[i];
+      derivadaGrado[i] = Grado[i]--;
+      derivadaValor[i] = Signo(derivadaCoeficiente, i);
+      Literales(derivadaCoeficiente, i, derivadaGrado, derivadaValor);
    }
    cout << endl;
 
    // empieza la solución de la ecuación
    // Utilizar if's para saber si x_n+1 + x_n == 0
 
-   int Precision;
-   cout << "Precision para el valor (iteraciones): ";
-   cin >> Precision;
-   float Iteracion[Precision], Funcion[Precision], FuncionDerivada[Precision];
+   int precision;
+   cout << "precision para el valor (iteraciones): ";
+   cin >> precision;
+   float iteracion[precision], funcion[precision], funcionDerivada[precision];
    cout << "Valor de primer iteracion: ";
-   cin >> Iteracion[0];
+   cin >> iteracion[0];
 
-   for (int j = 0; i < Precision; Precision++) {
-      for (int i = 0; i < Terminos; i++) {
-         Funcion[j] += Coeficiente[i] * pow(Iteracion[j],Grado[i]);
-         FuncionDerivada[j] += DerivadaCoeficiente[i] * pow(Iteracion[j], DerivadaGrado[i]);
+   for (int j = 0; i < precision; precision++) {
+      for (int i = 0; i < terminos; i++) {
+         funcion[j] += Coeficiente[i] * pow(iteracion[j],Grado[i]);
+         funcionDerivada[j] += derivadaCoeficiente[i] * pow(iteracion[j], derivadaGrado[i]);
       }
-      Iteracion[j + 1] = Iteracion[j] - (Funcion[j]/FuncionDerivada[j]);
+      iteracion[j + 1] = iteracion[j] - (funcion[j]/funcionDerivada[j]);
    }
-   cout << "Solucion mas cercana a " << Iteracion[0] << ": " << Iteracion[Precision];
+   cout << "Solucion mas cercana a " << iteracion[0] << ": " << iteracion[precision];
 }
