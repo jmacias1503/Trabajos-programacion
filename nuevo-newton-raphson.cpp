27153b1d987a6ae664d2d8c0adbb4643300008b1
Alejandro <alex.mac.fon@gmail.com>
2023-05-21 09:01:24 -0600

    problema de dar todas las soluciones resuelto?

--- nuevo-newton-raphson.cpp
+++ nuevo-newton-raphson.cpp
@@ -6,19 +6,44 @@ int main () {
    cout << "Grado de la ecuacion: ";
    cin >> g;
 
-   float c[g], dc[g];
+   float c[g+1], dc[g];
 
-   for (int i = 0 ; i < g; i++) {
+   for (int i = 0 ; i <= g; i++) {
       float de = g-1-i;
       cout << "Termino " << i + 1 << ": " << endl;
       cin >> c[i];
       cout << "\e[A" << c[i] << "x^" << g - i << endl;
-      dc[i] = c[i] * de;
    }
-   int t;
+   cout << "f'(x) = ";
+   for (int i = 0; i < g; i++) {
+      dc[i] = c[i] * (g - i);
+   }
+   cout << endl;
+   int t, i, j, k;
    cout << "tolerancia: ";
    cin >> t;
-   float x[t], f[t], df[t];
-   for (int i = 0; i < g; i++) {
+   float x[t], f[t], df[t], r[g];
+   for (i = 0; i <g; i++) {
+      cout << "Valor " << i + 1 << " a evaluar: ";
+      cin >> x[0];
+      for (j = 0; j <= t; j++) {
+         f[j] = 0;
+         df[j] = 0;
+         for (int k = 0; k <= g; k++) {
+            f[j] += c[k] * pow(x[j],(g-k));
+         }
+         for (k = 0; k < g; k++) {
+            df[j] += dc[k] * pow(x[j],(g-k));
+         }
+         x[j+ 1] = x[j] - (f[j]/df[j]);
+         if (x[j+1] - x[j] == 0) {
+            break;
+         }
+      }
+      r[i] = x[j];
+   }
+   cout << "Soluciones: " << endl << endl;
+   for (i = 0; i < g; i++) {
+      cout << "Solucion " << i + 1 << ": " << r[i] << endl;
    }
 }
