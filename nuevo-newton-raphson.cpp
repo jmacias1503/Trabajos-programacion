cb4610e256a2a9257b95dcbc02d6b38c747d465e
Alejandro <alex.mac.fon@gmail.com>
2023-05-21 10:09:59 -0600

    adicion de advertencia si se llega al mismo resultado

--- nuevo-newton-raphson.cpp
+++ nuevo-newton-raphson.cpp
@@ -43,6 +43,7 @@ int main () {
       r[i] = x[j];
       if (i != 0 && r[i] == r[i-1]) {
          i--;
+         cout << "\n INTRODUCE OTRO VALOR";
       }
    }
    cout << "Soluciones: " << endl << endl;
