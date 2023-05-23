d0cce5a7fe5044b78d23554cf1b947273275e313
Alejandro <alex.mac.fon@gmail.com>
2023-05-23 08:46:58 -0600

    reemplazo a nueva version

--- solucion-metodo-newton-raphson.cpp
+++ solucion-metodo-newton-raphson.cpp
@@ -1,107 +1,68 @@
-#include <cmath>
 #include <iostream>
-#include <string>
+#include <cmath>
 using namespace std;
-int Literales(float c[], int i, float e[], string v[]){
-   if (c[i] == 0) {
-      return 0;
-   }
-   if (i!=0) {
-      cout << v[i];
-   }
-      cout << abs(c[i]);
-   if (e[i] != 0) {
-      cout << "x";
-   }
-   if (e[i] != 0 && e[i] != 1) {
-      cout << "^" << e[i];
-   }
-   cout << " ";
-   return 0;
-}
-void SumaTerminos (float c[], float e[], int n, int i){
-
-   for (i = 0; i < n ; i++) {
-      for (int j = i + 1; j < n; j++) {
-         if (e[i] == e [j]) {
-            c[i] += c[j];
-            c[j] = 0;
-         }
-      }
-   }
-}
-
-string Signo(float c[], int i) {
-   string v = "+";
-   if (c[i] < 0) {
-      v = "-";
-   }
-   return v;
-}
-
-float FuncionIteracion(float c[], float e[], float x[], int n, int i, int j) {
-   float resultado;
-   for (i = 0; i < n; i++) {
-      resultado += c[i] * pow(x[j],e[i]);
-   }
-   return resultado;
-}
-float DerivadaIteracion(float dc[], float de[], float x[], int n, int i, int j) {
-   float resultadoDerivada;
-   for (i = 0; i < n; i++) {
-      resultadoDerivada += dc[i] * pow(x[j], de[i]);
-   }
-   return resultadoDerivada;
-}
-
 int main () {
-   int n, i;
-   cout << "Terminos: "; cin >> n;
-   float c[n], e[n], dc[n], de[n];
-   string v[n], derivadaValor[n];
-
-   for (i = 0; i < n; i++) {
-      cout << "Termino" << i + 1 << ": " << endl;
-      cin >> c[i]; 
-      cout <<"\e[A" << c[i] << "x^";
-      cin >> e[i];
-      v[i] = Signo( c, i);
-      cout << endl;
+   int g;
+   bool err = false;
+   cout << "Grado de la ecuacion: ";
+   cin >> g;
+   float c[g+1], dc[g];
+   bool imag[g];
+   for (int i = 0 ; i <= g; i++) {
+      float de = g-1-i;
+      cout << "Termino " << i + 1 << ": " << endl;
+      cin >> c[i];
+      cout << "\e[A" << c[i] << "x^" << g - i << endl;
    }
-   cout << "f(x) = ";
-   for (int i =0; i < n; i++) {
-   Literales(c, i, e, v);
-   }
-   cout << "\nf'(x) = " ;
-   for (i = 0; i < n; i++) {
-      dc[i] = c[i] * e[i];
-      de[i] = e[i] - 1;
-      v[i] = Signo(dc, i);
-      Literales(dc, i, de, derivadaValor);
+   cout << "f'(x) = ";
+   for (int i = 0; i < g; i++) {
+      dc[i] = c[i] * (g - i);
    }
    cout << endl;
-   // Empieza solución de la ecuación
-   int t;
+   int t, i, j, k;
    cout << "tolerancia: ";
    cin >> t;
-   float x[t], f[t], df[t];
-   cout << "Primer valor (distinto de 0): ";
-   cin >> x[0];
-   int j = 0;
-   f[j] = FuncionIteracion(c, e, x, n, i, j);
-   df[j] = DerivadaIteracion(dc, de, x, n, i, j);
-   cout << "f resuelta: " << FuncionIteracion(c, e, x, n, i, j) << "  " << f[j];
-   cout << endl << "f derivada: " << DerivadaIteracion(dc, de, x, n, i, j) << "  " << df[j] - f[j] << endl << endl;
-
-   for (j = 1; j <= t; j++) {
-      x[j] = x[j-1] - (f[j-1]/(df[j-1]-f[j-1]));
-      f[j] = FuncionIteracion(c, e, x, n, i, j);
-      df[j] = DerivadaIteracion(dc, de, x, n, i, j);
-      cout << "x[" << j << "] = " << x[j] << endl;
-      if (x[j] - x[j-1] == 0 || x[j] == 0) {
-         break;
+   float x[t], f[t], df[t], r[g];
+   for (i = 0; i <g; i++) {
+      cout << "Valor " << i + 1 << " a evaluar: ";
+      cin >> x[0];
+      for (j = 0; j <= t; j++) {
+         imag[i] = true;
+         f[j] = 0;
+         df[j] = 0;
+         for (int k = 0; k <= g; k++) {
+            f[j] += c[k] * pow(x[j],(g-k));
+         }
+         for (k = 0; k < g; k++) {
+            df[j] += dc[k] * pow(x[j],(g-k-1));
+         }
+         if (df[j] != 0) {
+            x[j+ 1] = x[j] - (f[j]/df[j]);
+            err = false;
+         }
+         else if (df[j] == 0){
+            err = true;
+         }
+         if (x[j+1] - x[j] == 0 || err == true) {
+            imag[i] = false;
+            break;
+         }
+      }
+      r[i] = x[j];
+      if ((i != 0 && r[i] == r[i-1]) || err == true) {
+         i--;
+         cout << "\nINTRODUCE OTRO VALOR\n";
       }
    }
-   cout << "Solucion mas cercana a " << x[0] << ": " << x[j];
-   cout << endl;
+      cout << "Soluciones: " << endl << endl;
+      for (i = 0; i < g; i++) {
+         cout << "Solucion " << i + 1 << ": ";
+         if (imag[i] == true) {
+            cout << "imaginaria";
+         }
+         else {
+            cout << r[i];
+         }
+         cout << endl;
+      }
 }
